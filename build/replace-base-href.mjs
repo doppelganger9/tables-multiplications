13b72d15415abcbe9b167320dc388213406ab6ed
David LACOURT <david.lacourt+github@gmail.com>
2023-01-30 22:12:19 +0100

    stupid typo broke the build again

--- build/replace-base-href.mjs
+++ build/replace-base-href.mjs
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
-import { existsSync, readfileSync, writeFileSync } from 'fs';
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 
 const staticIndexHtmlFile = './static/index.html';
 const searchThis = `<base href="/"`;
 const replaceWithThis = `<base href="/tables-multiplications/"`;
 
 if (existsSync(staticIndexHtmlFile)) {
-  const fileContents = readfileSync(staticIndexHtmlFile).toString();
+  const fileContents = readFileSync(staticIndexHtmlFile).toString();
   const changedFileContents = fileContents.replace(searchThis, replaceWithThis);
   writeFileSync(staticIndexHtmlFile, changedFileContents);
   console.log('replaced base url in static index.html file for GitHub Pages deployment');
