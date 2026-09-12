a7efcbcfe7b9ead4eb5ac9e90c48636075742eb4
David Lacourt <david.lacourt+github@gmail.com>
2026-09-12 19:07:48 +0200

    ajout synthèse vocale. (#548)

--- playwright/fix-409.spec.ts
+++ playwright/fix-409.spec.ts
@@ -1,17 +1,20 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from './fixtures';
 
-test('focuses and empties the response input for each next question', async ({ page }) => {
+test('focuses and empties the response input for each next question', async ({
+  page
+}) => {
   await page.goto('/');
   await page.locator('#select-action').selectOption({ label: 'Réviser' });
   await expect(page.locator('article > table')).toHaveCount(0);
 
   for (let questionIndex = 0; questionIndex < 3; questionIndex++) {
-    await page.locator('[data-e2e="btn-next-question"]').click();
+    if (questionIndex === 0) {
+      await page.locator('[data-e2e="btn-next-question"]').click();
+    }
     const question = page.locator('.question');
     await expect(question).toHaveText(/Combien font \d+ fois \d+ ?/);
-    const [, firstOperand, secondOperand] = (await question.textContent())!.match(
-      /Combien font (\d+) fois (\d+) ?/
-    )!;
+    const [, firstOperand, secondOperand] =
+      (await question.textContent())!.match(/Combien font (\d+) fois (\d+) ?/)!;
     const response = page.locator('#reponse');
     await expect(response).toBeVisible();
     await expect(response).toBeFocused();
@@ -20,5 +23,6 @@ test('focuses and empties the response input for each next question', async ({ p
     await page.locator('[data-e2e="btn-validate-answer"]').click();
     await expect(page.locator('[data-e2e="good-answer"]')).toBeVisible();
     await expect(page.locator('[data-e2e="bad-answer"]')).toHaveCount(0);
+    await expect(response).toBeFocused();
   }
 });
--- playwright/spec.spec.ts
+++ playwright/spec.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from './fixtures';
 
 test.describe('tables de multiplication', () => {
   test('visits the initial project page', async ({ page }) => {
@@ -13,7 +13,9 @@ test.describe('tables de multiplication', () => {
     await expect(actionSelect).toBeVisible();
     await expect(actionSelect).toHaveValue('Afficher');
     await expect(page.locator('article > table')).toBeVisible();
-    await expect(page.locator('article > table th').filter({ hasText: 'Opération' })).toBeVisible();
+    await expect(
+      page.locator('article > table th').filter({ hasText: 'Opération' })
+    ).toBeVisible();
     await expect(page.locator('[data-e2e="btn-next-question"]')).toHaveCount(0);
 
     await actionSelect.selectOption({ label: 'Réviser' });
@@ -27,20 +29,44 @@ test.describe('tables de multiplication', () => {
     const numberSelect = page.locator('#select-nombre');
     await numberSelect.selectOption('3');
     await expect(page.locator('article > table')).toBeVisible();
-    await expect(page.locator('tbody tr').nth(0).locator('td').nth(0)).toContainText('1 x 3');
-    await expect(page.locator('tbody tr').nth(0).locator('td').nth(1)).toContainText('3');
-    await expect(page.locator('tbody tr').nth(3).locator('td').nth(0)).toContainText('4 x 3');
-    await expect(page.locator('tbody tr').nth(3).locator('td').nth(1)).toContainText('12');
-    await expect(page.locator('tbody tr').nth(9).locator('td').nth(0)).toContainText('10 x 3');
-    await expect(page.locator('tbody tr').nth(9).locator('td').nth(1)).toContainText('30');
+    await expect(
+      page.locator('tbody tr').nth(0).locator('td').nth(0)
+    ).toContainText('1 x 3');
+    await expect(
+      page.locator('tbody tr').nth(0).locator('td').nth(1)
+    ).toContainText('3');
+    await expect(
+      page.locator('tbody tr').nth(3).locator('td').nth(0)
+    ).toContainText('4 x 3');
+    await expect(
+      page.locator('tbody tr').nth(3).locator('td').nth(1)
+    ).toContainText('12');
+    await expect(
+      page.locator('tbody tr').nth(9).locator('td').nth(0)
+    ).toContainText('10 x 3');
+    await expect(
+      page.locator('tbody tr').nth(9).locator('td').nth(1)
+    ).toContainText('30');
 
     await numberSelect.selectOption('10');
-    await expect(page.locator('tbody tr').nth(0).locator('td').nth(0)).toContainText('1 x 10');
-    await expect(page.locator('tbody tr').nth(0).locator('td').nth(1)).toContainText('10');
-    await expect(page.locator('tbody tr').nth(3).locator('td').nth(0)).toContainText('4 x 10');
-    await expect(page.locator('tbody tr').nth(3).locator('td').nth(1)).toContainText('40');
-    await expect(page.locator('tbody tr').nth(9).locator('td').nth(0)).toContainText('10 x 10');
-    await expect(page.locator('tbody tr').nth(9).locator('td').nth(1)).toContainText('100');
+    await expect(
+      page.locator('tbody tr').nth(0).locator('td').nth(0)
+    ).toContainText('1 x 10');
+    await expect(
+      page.locator('tbody tr').nth(0).locator('td').nth(1)
+    ).toContainText('10');
+    await expect(
+      page.locator('tbody tr').nth(3).locator('td').nth(0)
+    ).toContainText('4 x 10');
+    await expect(
+      page.locator('tbody tr').nth(3).locator('td').nth(1)
+    ).toContainText('40');
+    await expect(
+      page.locator('tbody tr').nth(9).locator('td').nth(0)
+    ).toContainText('10 x 10');
+    await expect(
+      page.locator('tbody tr').nth(9).locator('td').nth(1)
+    ).toContainText('100');
   });
 
   test('can answer questions', async ({ page }) => {
@@ -55,18 +81,18 @@ test.describe('tables de multiplication', () => {
 
     const question = page.locator('.question');
     await expect(question).toHaveText(/Combien font \d+ fois \d+ ?/);
-    const [, firstOperand, secondOperand] = (await question.textContent())!.match(
-      /Combien font (\d+) fois (\d+) ?/
-    )!;
-    await page.locator('#reponse').fill(String(Number(firstOperand) * Number(secondOperand)));
+    const [, firstOperand, secondOperand] =
+      (await question.textContent())!.match(/Combien font (\d+) fois (\d+) ?/)!;
+    await page
+      .locator('#reponse')
+      .fill(String(Number(firstOperand) * Number(secondOperand)));
     await page.locator('[name="submit"]').click();
     await expect(page.locator('[data-e2e="good-answer"]')).toBeVisible();
     await expect(page.locator('[data-e2e="bad-answer"]')).toHaveCount(0);
 
-    await page.locator('[data-e2e="btn-next-question"]').click();
-    await expect(page.locator('[data-e2e="good-answer"]')).toHaveCount(0);
-    await expect(page.locator('[data-e2e="bad-answer"]')).toHaveCount(0);
+    await expect(page.locator('#reponse')).toBeFocused();
     await expect(question).toHaveText(/Combien font \d+ fois \d+ ?/);
+    await expect(page.locator('[data-e2e="good-answer"]')).toHaveCount(0);
     await page.locator('#reponse').fill('438427');
     await page.locator('[name="submit"]').click();
     await expect(page.locator('[data-e2e="good-answer"]')).toHaveCount(0);
