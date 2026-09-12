edd23fd842cc75fc62bf43481bcc6d4d4a3a1895
David Lacourt <david.lacourt@gmail.com>
2026-09-12 16:54:55 +0200

    fix npm run lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,81 +1,85 @@
-import globals from "globals";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import angular from '@angular-eslint/eslint-plugin';
+import angularTemplate from '@angular-eslint/eslint-plugin-template';
+import templateParser from '@angular-eslint/template-parser';
+import tsParser from '@typescript-eslint/parser';
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
+export default [
+  {
     ignores: [
-        "projects/**/*",
-        "**/*package.json",
-        "**/package-lock.json",
-        "**/dist",
-        "**/coverage",
-        "e2e/**/*",
-        "**/commitlint.config.js*",
-    ],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@angular-eslint/recommended",
-    "plugin:@angular-eslint/template/process-inline-templates",
-    "plugin:prettier/recommended",
-).map(config => ({
-    ...config,
-    files: ["**/*.ts"],
-})), {
-    files: ["**/*.ts"],
-
+      'projects/**/*',
+      '**/*package.json',
+      '**/package-lock.json',
+      '**/dist',
+      '**/coverage',
+      'e2e/**/*',
+      '**/commitlint.config.js*',
+      'src/index.html'
+    ]
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
     languageOptions: {
-        globals: {
-            ...globals.browser,
-        },
-
-        ecmaVersion: 5,
-        sourceType: "script",
-
-        parserOptions: {
-            project: ["tsconfig.json", "e2e/tsconfig.json"],
-            createDefaultProgram: true,
-        },
+      parser: tsParser,
+      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        project: ['tsconfig.json', 'e2e/tsconfig.json'],
+        createDefaultProgram: true
+      }
     },
-
+    plugins: {
+      '@angular-eslint': angular,
+      prettier
+    },
+    processor: angularTemplate.processors['extract-inline-html'],
     rules: {
-        "no-unused-vars": "warn",
-
-        "@angular-eslint/component-selector": ["error", {
-            prefix: "app",
-            style: "kebab-case",
-            type: "element",
-        }],
-
-        "@angular-eslint/directive-selector": ["error", {
-            prefix: "app",
-            style: "camelCase",
-            type: "attribute",
-        }],
-
-        "sort-imports": ["error", {
-            ignoreCase: false,
-            ignoreDeclarationSort: true,
-            ignoreMemberSort: false,
-            memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
-            allowSeparatedGroups: false,
-        }],
-
-        "no-console": "off",
+      'no-unused-vars': 'warn',
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          prefix: 'app',
+          style: 'kebab-case',
+          type: 'element'
+        }
+      ],
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          prefix: 'app',
+          style: 'camelCase',
+          type: 'attribute'
+        }
+      ],
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: true,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: false
+        }
+      ],
+      'no-console': 'off',
+      'prettier/prettier': 'error'
+    }
+  },
+  {
+    files: ['**/*.html'],
+    languageOptions: {
+      parser: templateParser
     },
-}, ...compat.extends("plugin:@angular-eslint/template/recommended").map(config => ({
-    ...config,
-    files: ["**/*.html"],
-})), {
-    files: ["**/*.html"],
-    rules: {},
-}];
+    plugins: {
+      '@angular-eslint/template': angularTemplate,
+      prettier
+    },
+    rules: {
+      'prettier/prettier': 'error'
+    }
+  }
+];
